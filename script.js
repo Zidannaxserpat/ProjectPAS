0fb01d1b364f78e8c84715df01d0f826bb8535b3
Zidannaxserpat <134835974+Zidannaxserpat@users.noreply.github.com>
2023-12-03 10:35:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function ATM(){
     }
   }
 
-  // Loop untuk memilih apakah ingin login atau membuat akun
+  // Loop untuk memilih mau login apa buat akun
   while (true) {
     let pilihan = prompt('Pilih tindakan:\n1. Login\n2. Buat akun\n3. Keluar');
 
@@ -102,8 +102,12 @@ function ATM(){
       wadahAkun.push(akunBaru);
       alert('Akun berhasil ditambahkan!');
     } else if (pilihan === '3') {
-      confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit"));
+      confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
       loop();
+
+      if(confirm == 0) {
+        break;
+      }
     } else {
       alert('Pilihan tidak valid. Silakan pilih lagi.');
     }
@@ -111,109 +115,128 @@ function ATM(){
 }
 
 function tebakAngka() {
-  let ulang = true;
-  while (ulang) {
-    let chance = 3;
-    let c = Math.floor(Math.random() * 10) + 1;
-    let hasil = '';
+  let chance = 3;
+  let c = Math.floor(Math.random() * 10) + 1;
+  let hasil = '';
 
-    while (chance >= 1) {
-      alert("Memulai permainan dengan " + chance + " kesempatan");
-      let p = parseInt(prompt("Tebak angka 1-10"));
-      if (p === c) {
-        hasil = 'BENAR';
-        alert('Anda ' + hasil + ' angka yang anda tebak adalah ' + c);
-        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit"));
-        loop();
-      } else if (p < c) {
-        hasil = 'RENDAH';
-        chance -= 1;
-        alert('Terlalu ' + hasil + ' angka yang anda tebak adalah ' + p + '\ntersisa ' + chance + ' kesempatan lagi.');
-      } else if (p > c) {
-        hasil = 'TINGGI';
-        chance -= 1;
-        alert('Terlalu ' + hasil + ' angka yang anda tebak adalah ' + p + '\ntersisa ' + chance + ' kesempatan lagi.');
-      }
-
-      if (chance === 0) {
-        alert('Anda gagal, angka yang benar adalah ' + c);
-        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit"));
-        loop();
-      }
+  while (chance >= 1) {
+    alert("Memulai permainan dengan " + chance + " kesempatan");
+    let p = parseInt(prompt("Tebak angka 1-10"));
+    if (p === c) {
+      hasil = 'BENAR';
+      alert('Anda ' + hasil + ' angka yang anda tebak adalah ' + c);
+      break;  
+    } else if (p < c) {
+      hasil = 'RENDAH';
+      chance -= 1;
+      alert('Terlalu ' + hasil + ' angka yang anda tebak adalah ' + p + '\ntersisa ' + chance + ' kesempatan lagi.');
+    } else if (p > c) {
+      hasil = 'TINGGI'; 
+      chance -= 1;
+      alert('Terlalu ' + hasil + ' angka yang anda tebak adalah ' + p + '\ntersisa ' + chance + ' kesempatan lagi.');
     }
-    ulang = confirm('Mau ulang ga?');
-  }
+
+    if (chance === 0) {
+      alert('Anda gagal, angka yang benar adalah ' + c);
+    }
+  }  
   alert("Oke makasih udah main");
+  confirm = parseInt(prompt("Pilih program:\n\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
 }
 
-let confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit"));
+function ganjilGenap() {
+  let angka = parseInt(prompt("Masukkan angka"));
+        
+  (angka % 2 == 0) ? alert("Anda memasukkan angka Genap!") : alert("Anda memasukkan angka ganjil!");
+  confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
+}
+
+function Suwit() {
+  while(true) {
+
+    let playerInput = prompt("Pilih batu, gunting, kertas / keluar:");
+    let player = playerInput.toLowerCase(); // Convert input to lowercase
+    let bot = Math.random();
+        
+    if(bot < 0.34){
+      bot = 'batu';
+    }else if(bot > 0.33 && bot <= 0.66){
+      bot = 'gunting';
+    }else{
+      bot = 'kertas';
+    }
+        
+    let hasil;
+        
+    if(player == bot){
+      hasil = 'Hasil permainan seri';
+    }else if(bot == 'kertas'){
+      if(player == 'gunting'){
+        hasil = 'Anda Menang';
+      }else if(player == 'batu'){
+         hasil = 'Anda kalah';
+      }else if(player == "keluar") {
+        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
+        loop();
+        
+        if(confirm == 0) {
+          break;
+        }
+      }else{
+        alert("Salah input!")
+        loop();
+      }
+    }else if(bot == 'batu'){
+      if(player == 'gunting'){
+        hasil = 'Anda kalah';
+      }else if(player == 'kertas'){
+        hasil = 'Anda Menang';
+      }else if(player == "keluar") {
+        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
+        loop();
+        
+        if(confirm == 0) {
+          break;
+        }
+      }else{
+        alert("Salah input!")
+        loop();
+      }
+    }else if(bot == 'gunting'){
+      if(player == 'batu'){
+        hasil = 'Anda Menang';
+      }else if(player == 'kertas'){
+        hasil = 'Anda kalah';
+      }else if(player == "keluar") {
+        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
+        loop();
+        
+        if(confirm == 0) {
+          break;
+        }
+      }else{
+        alert("Salah input!")
+        loop();
+      }
+    }  
+    alert("Bot memilih "+bot+" sedangkan kamu milih "+player+" hasil permainan: "+hasil);
+  }
+}
+
+let confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit\n0. Keluar"));
 
 function loop() {
-  while(confirm == 1 || confirm == 2 || confirm == 3 || confirm == 4) {
+  while(confirm == 1 || confirm == 2 || confirm == 3 || confirm == 4 || confirm == 0) {
     if(confirm == 1) {
       ATM();
-      if(pilihan == '3'){
-        break;
-      }
     }else if(confirm == 2) {
       tebakAngka();
     }else if(confirm == 3) {
-        let angka = parseInt(prompt("Masukkan angka"));
-        
-        (angka % 2 == 0) ? alert("Anda memasukkan angka Genap!") : alert("Anda memasukkan angka ganjil!");
-        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit"));
+      ganjilGenap();
     }else if(confirm == 4) {
-      let playerInput = prompt("Pilih batu, gunting, kertas / keluar:");
-      let player = playerInput.toLowerCase(); // Convert input to lowercase
-  
-      
-      let bot = Math.random();
-      
-      if(bot < 0.34){
-        bot = 'batu';
-      }else if(bot > 0.33 && bot <= 0.66){
-        bot = 'gunting';
-      }else{
-        bot = 'kertas';
-      }
-      
-      
-      let hasil;
-      
-      if(player == bot){
-        hasil = 'Hasil permainan seri';
-      }else if(player == "keluar") {
-        confirm = parseInt(prompt("Pilih program:\n1. ATM\n2. Game tebak angka\n3. Ganjil / genap\n4. Suwit"));
-        loop();
-      }else if(bot == 'kertas'){
-        if(player == 'gunting'){
-          hasil = 'Anda Menang';
-        }else if(player == 'batu'){
-          hasil = 'Anda kalah';
-        }else{
-          alert("Salah input!")
-          loop();
-        }
-      }else if(bot == 'batu'){
-        if(player == 'gunting'){
-          hasil = 'Anda kalah';
-        }else if(player == 'kertas'){
-          hasil = 'Anda Menang';
-        }else{
-          alert("Salah input!")
-          loop();
-        }
-      }else if(bot == 'gunting'){
-        if(player == 'batu'){
-          hasil = 'Anda Menang';
-        }else if(player == 'kertas'){
-          hasil = 'Anda kalah';
-        }else{
-          alert("Salah input!")
-          loop();
-        }
-      }  
-      alert("Bot memilih "+bot+" sedangkan kamu milih "+player+" hasil permainan: "+hasil);
+      Suwit();
+    }else if(confirm == 0) {
+      break;
     }
   }
 }
